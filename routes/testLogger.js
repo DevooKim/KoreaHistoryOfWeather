6373c1157080474d20b90800bab9b8d7ed81de5f
DevooKim <khwoo8026@gmail.com>
2020-12-01 15:58:07 +0900

    test

--- routes/testLogger.js
+++ routes/testLogger.js
@@ -1,10 +1,10 @@
-const rp = require('request-promise-native')
 const express = require('express')
-const { getYesterdays, befores, forecasts } = require('./middlewares_old')
+const { isCache } = require('./func/cache')
+const { getYesterdays, getBefores, getForecasts } = require('./middlewares')
 const dayjs = require('dayjs')
 const UTC = require('dayjs/plugin/utc')
 const timezone = require('dayjs/plugin/timezone')
-const winston = require('../config/winston')
+const winston = require('../config/winston');
 
 dayjs.extend(UTC);
 dayjs.extend(timezone);
@@ -17,34 +17,18 @@ const weathers = {
     "todays": [],
     "tomorrows": [],
 }
-
+    
 //lat, lon: 36.354687/127.420997
-router.get('/:lat/:lon', getYesterdays, befores, forecasts, async (req, res) => {
+router.get('/:lat/:lon', isCache, getYesterdays, getBefores, getForecasts, async (req, res) => {
 
-    const kor = dayjs.tz();
-    winston.info("now" + kor.format())
-
-    const data = [...req.yesterdays, ...req.befores, ...req.forecasts]
-
-    winston.info("yesterdays");
-    weathers.yesterdays = print(data.slice(5, 13));
-    winston.info("todays");
-    weathers.todays = print(data.slice(13, 21));
-    winston.info("tomorrows");
-    weathers.tomorrows = print(data.slice(21, 30));
+    let data = [...req.yesterdays, ...req.befores, ...req.forecasts]
+    weathers.yesterdays = data.slice(5, 13);
+    weathers.todays = data.slice(13, 21);
+    weathers.tomorrows = data.slice(21, 30);
 
     res.send(weathers);
 
 });
-
-function print(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        winston.info(dayjs.unix(arr[i].dt).tz().format());
-        arr[i] = dayjs.unix(arr[i].dt).tz().hour()
-    }
-
-    return arr;
-}
 
 
 module.exports = router;
