4281fe73886567ebc51ea18f61fadc62aa699ba8
DevooKim <khwoo8026@gmail.com>
2020-12-08 11:13:55 +0900

    .

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const morgan = require('morgan')
-const dotenv = require('dotenv')
-const path = require('path')
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const path = require("path");
 
-const weatherRouter = require('./routes/weatherRouter')
+const weatherRouter = require("./routes/weatherRouter");
 
 dotenv.config();
 
 const app = express();
-app.set('port', process.env.PORT || 8001);
+app.set("port", process.env.PORT || 8001);
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/weather', weatherRouter);
+app.use("/weather", weatherRouter);
 
 app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`); //추후 제거
-    error.status = 404;
-    next(error);
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`); //추후 제거
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.send(err);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.send(err);
 });
 
-app.listen(app.get('port'), (err) => {
-    if (!err) {
-        console.log(app.get('port'), '번 포트에서 대기 중');
-        //loggingRouter;
-    } else {
-        console.log('서버 오류');
-        throw new Error('서버 오류', err);
-    }
+app.listen(app.get("port"), (err) => {
+  if (!err) {
+    console.log(app.get("port"), "번 포트에서 대기 중");
+    //loggingRouter;
+  } else {
+    console.log("서버 오류");
+    throw new Error("서버 오류", err);
+  }
 });
--- routes/weatherRouter.js
+++ routes/weatherRouter.js
@@ -1,29 +1,28 @@
-const express = require('express')
-const { isCache } = require('./func/cache')
-const { getWeathers } = require('./middlewares')
-const winston = require('../config/winston')
+const express = require("express");
+const { isCache } = require("./func/cache");
+const { getWeathers } = require("./middlewares");
+const winston = require("../config/winston");
 
 const router = express.Router();
 
 const weathers = {
-    "yesterdays": [],
-    "todays": [],
-    "tomorrows": [],
-    "daily": [],
-}
-    
+  yesterdays: [],
+  todays: [],
+  tomorrows: [],
+  daily: [],
+};
+
 //lat, lon: 36.354687/127.420997
-// router.get('/:lat/:lon', isCache, getWeathers, async (req, res) => {
-router.post('/', isCache, getWeathers, async (req, res) => {
+router.get("/:lat/:lon", isCache, getWeathers, async (req, res) => {
+  // router.post('/', isCache, getWeathers, async (req, res) => {
 
-    let data = [...req.yesterdays, ...req.befores, ...req.forecasts]
-    weathers.yesterdays = data.slice(5, 13);
-    weathers.todays = data.slice(13, 21);
-    weathers.tomorrows = data.slice(21, 30);
-    weathers.daily = req.daily;
+  let data = [...req.yesterdays, ...req.befores, ...req.forecasts];
+  weathers.yesterdays = data.slice(5, 13);
+  weathers.todays = data.slice(13, 21);
+  weathers.tomorrows = data.slice(21, 30);
+  weathers.daily = req.daily;
 
-    res.send(weathers);
-
+  res.send(weathers);
 });
 
 module.exports = router;
